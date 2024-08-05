082241df807f05f9b464651c06b3e234e7b45a8b
Claire Carter <carter.claire.e@gmail.com>
2024-08-05 10:58:57 -0700

    Added init db notes

--- model/init_db.sql
+++ model/init_db.sql
@@ -2,17 +2,20 @@
 -- Drop Tables
 --
 
-SET foreign_key_checks = 0;
+-- SET foreign_key_checks = 0;
+-- CHANGE THESE TO MATCH TABLES BELOW FROM DRAW SQL
 DROP TABLE if exists students;
-SET foreign_key_checks = 1;
+-- SET foreign_key_checks = 1;
 
 --
 -- Create Tables
 --
-
+-- CHANGE THESE WITH DRAW SQL CODE
 CREATE TABLE students(
     id INT NOT NULL AUTO_INCREMENT, 
     firstname VARCHAR(40) not null, 
     lastname VARCHAR(40) not null, 
     PRIMARY KEY (id)
     );
+
+-- npm run migrate after doing all of this
