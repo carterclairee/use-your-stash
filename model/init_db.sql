51eb1ccfb4de2181aabc72d9ca232d3ffae71857
Claire Carter <carter.claire.e@gmail.com>
2024-08-14 14:39:50 -0700

    Added database setup

--- model/init_db.sql
+++ model/init_db.sql
@@ -4,18 +4,42 @@
 
 -- SET foreign_key_checks = 0;
 -- CHANGE THESE TO MATCH TABLES BELOW FROM DRAW SQL
-DROP TABLE if exists students;
+-- First must drop junction table as it is child of yarn and patterns
+DROP TABLE if exists yarn_patterns;
+DROP TABLE if exists yarn;
+DROP TABLE if exists patterns;
 -- SET foreign_key_checks = 1;
 
 --
 -- Create Tables
 --
 -- CHANGE THESE WITH DRAW SQL CODE
-CREATE TABLE students(
-    id INT NOT NULL AUTO_INCREMENT, 
-    firstname VARCHAR(40) not null, 
-    lastname VARCHAR(40) not null, 
-    PRIMARY KEY (id)
+CREATE TABLE yarn (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(255) not null, 
+    brand VARCHAR(255) not null, 
+    weight VARCHAR(255) not null, 
+    yardage BIGINT not null, 
+    color VARCHAR(255) not null, 
+    fiber_type VARCHAR(255) not null
+    );
+
+CREATE TABLE patterns (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) not null, 
+    brand VARCHAR(255) not null, 
+    project_type VARCHAR(255) NOT NULL,
+    yardage_needed BIGINT NOT NULL,
+    yarn_weight VARCHAR(255) NOT NULL,
+    notes VARCHAR(255) NULL,
+    difficulty VARCHAR(255) NULL
+    );
+
+CREATE TABLE yarn_patterns (
+    yarn_id INT NOT NULL,
+    pattern_id INT NOT NULL,
+    FOREIGN KEY (yarn_id) REFERENCES yarn(id),
+    FOREIGN KEY (pattern_id) REFERENCES patterns(id)
     );
 
 -- npm run migrate after doing all of this
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const db = require("..model/helper");
+
+// Full url for the root is http://localhost:4000/api
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
