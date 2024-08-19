bd1371b6c7bb540e78a8128e62ff3443c1d28a6e
Claire Carter <carter.claire.e@gmail.com>
2024-08-19 13:06:04 -0700

    Added text and styling to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 import {Routes, Route, Link} from 'react-router-dom';
+import logo from './assets/icons8-yarn-80.png';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <div className="d-flex container-fluid bg-dark text-white">
       <div>
-      This will be a yarn stash app
+        <img className="logo m-2" src={logo} alt="yarn icon"/>
       </div>
+      <h1 className="m-2 align-self-center">Use Your Stash</h1>
+      <p className="m-2 ms-5 top-links align-self-center">yarn</p>
+      <p className="m-2 ms-5 top-links align-self-center">patterns</p>
+    </div>
     </>
   )
 }
